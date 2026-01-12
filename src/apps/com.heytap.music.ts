05ebaeda347eddd7667f095263d44921446a1c29
Liberations <1487098940@qq.com>
2026-01-12 11:22:53 +0800

    update

--- src/apps/com.heytap.music.ts
+++ src/apps/com.heytap.music.ts
@@ -17,7 +17,7 @@ export default defineGkdApp({
     {
       key: 3,
       name: '解锁30分钟关闭',
-      rules: 'android.widget.ImageView[id="id="com.heytap.music:id/iv_close"]',
+      rules: 'android.widget.ImageView[id="com.heytap.music:id/iv_close"]',
     },
   ],
 });
